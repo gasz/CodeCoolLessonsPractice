e26bc28148f28b1cfee5a421361234f392702be8
szega <gaborszekeres44@gmail.com>
2021-11-04 08:15:24 +0100

    This commit will for the second task.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("\nFirst Task");
 	    Week4.firstTask();
+
+        System.out.println("\nSecond Task");
+        Week4.secondTask();
+
     }
 }
--- src/com/company/Week4.java
+++ src/com/company/Week4.java
@@ -4,10 +4,17 @@ public class Week4 {
 
     static void firstTask(){
         for (int i = 0; i <= 100; i++) {
-            System.out.print(" " + i);
+            System.out.print(i + " ");
             if ( i % 20 == 0 && i != 0 )
                 System.out.println();
         }
     }
 
+    static void secondTask(){
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
 }
