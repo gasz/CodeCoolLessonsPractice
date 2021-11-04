ae8d9624bb596671a703a6262f0a29e958b1b5a7
szega <gaborszekeres44@gmail.com>
2021-11-04 11:44:48 +0100

    This commit will the third task.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\nFirst Task");
+/*        System.out.println("\nFirst Task");
 	    Week4.firstTask();
 
         System.out.println("\nSecond Task");
-        Week4.secondTask();
+        Week4.secondTask();*/
+
+        System.out.println("\nThird Task");
+        Week4.fibonacciUnder100();
 
     }
 }
--- src/com/company/Week4.java
+++ src/com/company/Week4.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Week4 {
 
     static void firstTask(){
@@ -16,5 +18,21 @@ public class Week4 {
             sum += i;
         }
         System.out.println(sum);
+    }
+
+    static void fibonacciUnder100(){
+
+        int numberN, numberNMinus1=1, numberNMinus2=1;
+
+        do {
+            numberN = numberNMinus1 + numberNMinus2;
+
+            if ( numberN < 100 )
+                System.out.print(numberN + ", ");
+
+            numberNMinus2 = numberNMinus1;
+            numberNMinus1 = numberN;
+
+        } while( numberN < 100 );
     }
 }
