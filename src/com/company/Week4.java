55865cd20f13fed08049f052328b3db41186c43a
szega <gaborszekeres44@gmail.com>
2021-11-04 07:57:58 +0100

    This commit will the first task

---
+++ src/com/company/Week4.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Week4 {
+    static void firstTask(){
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(" " + i);
+            if ( i % 20 == 0 )
+                System.out.println();
+        }
+    }
+}
